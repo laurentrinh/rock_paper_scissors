c082103d21395c170f90b921a1abda77a9d1edb6
laurentrinh <47019451+laurentrinh@users.noreply.github.com>
2019-07-31 16:12:07 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
-let computer = 0;
-let player = 0;
-let round = 0;
-let tie = 0;
+let computer = 0,
+    player = 0,
+    round = 0,
+    tie = 0;
 
 function computerPlay() {
-    let computerResult = ["rock", "paper", "scissors"];
+    let computerResult = ["Rock", "Paper", "Scissors"];
     return computerResult[Math.floor(Math.random()*3)];
 }
 
+function resetButton() {
+    document.getElementById("scoring").reset();
+}
+
 function playRound(playerSelection) {
-    const text = document.getElementById('text');
-    const roundNum = document.getElementById('roundNum');
-    const playerWin = document.getElementById('playerWin');
-    const compWin = document.getElementById('compWin');
-    const ties = document.getElementById('ties');
+    const text = document.querySelector("#text");
+    const roundNum = document.querySelector("#roundNum");
+    const playerWin = document.querySelector("#playerWin");
+    const compWin = document.querySelector("#compWin");
+    const ties = document.querySelector("#ties");
 
     let computerSelection = computerPlay();
     round++;
@@ -24,16 +28,16 @@ function playRound(playerSelection) {
         text.textContent = "Tie!"
         ties.textContent = tie;
     }
-    else if ((playerSelection == "rock" && computerSelection == "paper") ||
-            (playerSelection == "paper" && computerSelection == "scissors") ||
-            (playerSelection == "scissors" && computerSelection == "rock")) {
+    else if ((playerSelection == "Rock" && computerSelection == "Paper") ||
+            (playerSelection == "Paper" && computerSelection == "Scissors") ||
+            (playerSelection == "Scissors" && computerSelection == "Rock")) {
                 computer++;
-                text.textContent = " "+computerSelection+ " beats " +playerSelection+"!";
+                text.textContent = ""+computerSelection+" beats "+playerSelection+"!";
                 compWin.textContent = computer;
             }
     else {
         player++;
-        text.textContent = " "+playerSelection+ " beats " +computerSelection+"!";
+        text.textContent = ""+playerSelection+" beats "+computerSelection+"!";
         playerWin.textContent = player;
     }
 
@@ -49,7 +53,3 @@ function gameCheck() {
     }
 } 
 
-const buttons = document.querySelectorAll('button');
-buttons.forEach((button) => {
-    button.addEventListener('click', playRound);
-});
